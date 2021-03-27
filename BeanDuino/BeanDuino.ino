8f32cd50fb877e139e6bc93c42b5a8536c7c0886
Sam <samuel.sudbury@gmail.com>
2021-03-26 20:32:54 -0400

    BeanDuino

--- BeanDuino/BeanDuino.ino
+++ BeanDuino/BeanDuino.ino
@@ -1,3 +1,28 @@
+// BeanDuino
+
+unsigned long millisTime;
+int displayTime = 3000;
+int lightFirstDigit;
+int lightFirstDigitClone;
+int lightSecondDigit;
+int lightThirdDigit;
+
+#include "SevSeg.h"
+SevSeg sevseg; 
+
+void shutdownDisplay() {
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
+
+
 // Water level stuff
 int resVal; // Sensor Value
 int resPin = A5; // Sensor Pin
@@ -29,6 +54,9 @@ void readWaterLevel() {
 }
 // This function will read the water level
 
+// LED setup stuff
+int ledPin = 13;
+
 // Execute Order Photoresistor stuff
 int lightLevel;
 int lightPin = A0;
@@ -36,24 +64,139 @@ int lightPin = A0;
 void readLightLevel() {
   lightLevel = analogRead(lightPin);
   Serial.println(lightLevel);
+  if (lightLevel < 530) {
+    
+    while (lightLevel < 530) {
+      lightLevel = analogRead(lightPin);
+      Serial.println(lightLevel);
+      Serial.println("Where is the sun!?");
+      digitalWrite(ledPin, LOW);
+      delay(dt);
+      lightLevel = lightLevel * 0.01;
+      Serial.println(lightLevel);
+      sevseg.setNumber(lightLevel);
+      sevseg.refreshDisplay();
+      lightLevel = analogRead(lightPin);
+    }
+    digitalWrite(ledPin, HIGH);
+  }
 }
 // This function will read the light level
 
-// LED setup stuff
-int ledPin = 13;
-
 void setup() {
   // put your setup code here, to run once:
+
+  byte numDigits = 1;
+  byte digitPins[] = {};
+  byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
+  bool resistorsOnSegments = true;
+
+  byte hardwareConfig = COMMON_CATHODE; 
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
+
+    
   pinMode(resPin, INPUT); // PINMODE!!! I almost forgot about this.
   pinMode(lightPin, INPUT); // Pinmode for Photoresistor
   pinMode(ledPin, OUTPUT); // Pinmode for LED
   Serial.begin(9600); // Start the serial monitor (good ol' 9600)
+  digitalWrite(ledPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  readLightLevel();
-  digitalWrite(ledPin, LOW);
   
+  
+  millisTime = millis() + displayTime;
+  while (millisTime > millis()) {
+    lightLevel = analogRead(lightPin);
+    lightFirstDigit = lightLevel * 0.01;
+    lightFirstDigitClone = lightFirstDigit * 10;
+    //Serial.println(lightFirstDigit);
+    sevseg.setNumber(lightFirstDigit, 0);
+    sevseg.refreshDisplay();
+
+    //Serial.println("1");
+      
+    lightLevel = analogRead(lightPin);
+    Serial.println(lightFirstDigitClone);
+    if (lightLevel < 530) {
+      
+      digitalWrite(ledPin, LOW);
+     }
+  
+     else {
+      digitalWrite(ledPin, HIGH);
+     }
+  }  
+
+  millisTime = millis() + displayTime;
+  while (millisTime > millis()) {
+    
+    lightFirstDigit = lightFirstDigit * 100;
+    lightSecondDigit = lightLevel - lightFirstDigit;
+    
+    lightSecondDigit = lightSecondDigit * 0.1;
+    //lightFirstDigitClone = lightFirstDigitClone * 10;
+    lightSecondDigit = lightSecondDigit - lightFirstDigitClone;
+    
+    //Serial.println(lightSecondDigit);
+    Serial.println(lightFirstDigitClone);
+    sevseg.setNumber(lightSecondDigit);
+    sevseg.refreshDisplay();
+
+    //Serial.println("2");
+    
+      
+    lightLevel = analogRead(lightPin);
+    //Serial.println(lightLevel);
+    if (lightLevel < 530) {
+      
+      digitalWrite(ledPin, LOW);
+     }
+  
+     else {
+      digitalWrite(ledPin, HIGH);
+     }
+  }  
+
+  millisTime = millis() + displayTime;
+  while (millisTime > millis()) {
+    lightSecondDigit = lightSecondDigit * 10;
+    lightThirdDigit = lightFirstDigit + lightSecondDigit;
+    //Serial.println(lightThirdDigit);
+    sevseg.setNumber(lightThirdDigit, 0);
+    sevseg.refreshDisplay();
+
+    //Serial.println("3");
+      
+    lightLevel = analogRead(lightPin);
+    //Serial.println(lightLevel);
+    if (lightLevel < 530) {
+      
+      digitalWrite(ledPin, LOW);
+     }
+  
+     else {
+      digitalWrite(ledPin, HIGH);
+     }
+  }  
+
+  millisTime = millis() + 2000;
+  while (millisTime > millis()) {
+    shutdownDisplay();
+
+    //Serial.println("4");
+      
+    lightLevel = analogRead(lightPin);
+    Serial.println(lightLevel);
+    if (lightLevel < 530) {
+      
+      digitalWrite(ledPin, LOW);
+     }
+  
+     else {
+      digitalWrite(ledPin, HIGH);
+     }
+  }  
 }
