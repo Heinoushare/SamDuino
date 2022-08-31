9d004bee222b2de5c7d09ef1a13e53bf12e54346
Heinoushare <samuel.sudbury@gmail.com>
2022-08-31 08:42:54 -0400

    Runs with LCD

--- StackmatV2SensorTest/StackmatV2SensorTest.ino
+++ StackmatV2SensorTest/StackmatV2SensorTest.ino
@@ -1,7 +1,12 @@
 #include <CapacitiveSensor.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 CapacitiveSensor rPad = CapacitiveSensor(4, 8);
 CapacitiveSensor lPad = CapacitiveSensor(4, 6);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 
 long rVal = 0;
 long lVal = 0;
@@ -10,6 +15,10 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
+  // LCD Setup
+  lcd.init();     
+  lcd.backlight();
+  lcd.setCursor(0,0);
 
 }
 
@@ -23,5 +32,15 @@ void loop() {
   Serial.println(rVal);
   Serial.print("lVal: ");
   Serial.println(lVal);
+
+  lcd.clear();
+
+  lcd.print("rVal: ");
+  lcd.print(rVal);
+  lcd.setCursor(0, 1);
+  lcd.print("lVal: ");
+  lcd.print(lVal);
+
+  delay(100);
 
 }
