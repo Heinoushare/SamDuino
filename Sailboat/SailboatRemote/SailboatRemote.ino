9728bc7d6985c5e1a4f40ec3a83344e9407ba01b
Heinoushare <samuel.sudbury@gmail.com>
2023-09-04 08:39:19 -0400

    fasbter comms

--- Sailboat/SailboatRemote/SailboatRemote.ino
+++ Sailboat/SailboatRemote/SailboatRemote.ino
@@ -31,6 +31,19 @@ int Spin = 12;
 int Xval;
 int Yval;
 int Sval;
+int oldXval = 0;
+int oldYval = 0;
+int oldSval = 1;
+
+char Xpos = '-';
+char Ypos = '-';
+
+char oldXPos = '-';
+char oldYPos = '-';
+
+bool Xchange = false;
+bool Ychange = false;
+bool Schange = false;
 
 String oldMsg = "0,0,1;";
 
@@ -53,28 +66,106 @@ void loop() {
   Yval = analogRead(Ypin);
   Sval = digitalRead(Spin);
 
-  delay(50);
+  if (abs(511 - Yval) < 450) {
+    Ypos = '-';
+  }
+  else if (Yval > 511) {
+    Ypos = 'u';
+  }
+  else if (Yval < 511) {
+    Ypos = 'd';
+  }
 
   if (abs(511 - Xval) < 450) {
+    Xpos = '-';
+  }
+  else if (Xval > 511) {
+    Xpos = 'u';
+  }
+  else if (Xval < 511) {
+    Xpos = 'd';
+  }
+
+  delay(50);
+
+  if (oldSval != Sval) {
+    
+    if (Sval == 0) {
+      Serial.write('e');
+      oldSval = Sval;
+      return;
+    }
+    else if (Sval == 1) {
+      Serial.write('E');
+      oldSval = Sval;
+      return;
+    }
+    
+  }
+
+  if (Ypos == oldYPos) {
+    
+  }
+  else if (abs(511 - Yval) < 450) {
+    Yval = 511;
+    Ychange = true;
+  }
+  else if (Yval > 511) {
+    oldYval = Yval;
+    oldYPos = Ypos;
+    Serial.write('R');
+    Ychange = true;
+    return;
+  }
+  else if (Yval < 511) {
+    oldYval = Yval;
+    oldYPos = Ypos;
+    Serial.write('r');
+    Ychange = true;
+    return;
+  }
+
+  if (Xpos == oldXPos) {
+    
+  }
+  else if (abs(511 - Xval) < 450) {
     Xval = 511;
   }
-
-  if (abs(511 - Yval) < 450) {
-    Yval = 511;
+  else if (Xval > 511) {
+    oldXval = Xval;
+    oldXPos = Xpos;
+    Serial.write('S');
+    return;
+  }
+  else if (Xval < 511) {
+    oldXval = Xval;
+    oldXPos = Xpos;
+    Serial.write('s');
+    return;
   }
 
-  String sailVal = String(round((511 - Xval) / 1023. * 2.));
-  String rudderVal = String(round((511 - Yval) / 1023. * 2.));
-  String btnVal = String(Sval);
-  String msg = sailVal + "," + rudderVal + "," + btnVal + ";";
+  if (Sval == 1 && abs(511 - Xval) < 450 && abs(511 - Yval) < 450 && (oldXPos != '-' || oldYPos != '-' || oldSval != 1)) {
+    Serial.write('-');
+  }
+  oldXval = Xval;
+  oldYval = Yval;
+  oldSval = Sval;
+
+  oldYPos = Ypos;
+  oldXPos = Xpos;
+
+//  String sailVal = String(round((511 - Xval) / 1023. * 2.));
+//  String rudderVal = String(round((511 - Yval) / 1023. * 2.));
+//  String btnVal = String(Sval);
+//  String msg = sailVal + "," + rudderVal + "," + btnVal + ";";
   
 //  Serial.println("Sail: " + sailVal);
 //  Serial.println("Rudder: " + rudderVal);
 //  Serial.println("Switch: " + btnVal);
 //  Serial.println();
 
-  if (oldMsg.equals(msg) == false) {
-    Serial.println(sailVal + "," + rudderVal + "," + btnVal);
-    oldMsg = msg;
-  }
+//  if (oldMsg.equals(msg) == false) {
+//    Serial.println(sailVal + "," + rudderVal + "," + btnVal);
+//    oldMsg = msg;
+//  }
 }
--- Sailboat/SailboatSketch/SailboatSketch.ino
+++ Sailboat/SailboatSketch/SailboatSketch.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+//#include "ArduinoLowPower.h"
 
 int rudderPin = 3;
 int rudderAngle = 80;
@@ -7,9 +8,10 @@ Servo rudder;
 
 const int servoChange = 1;
 
+
 int sailPin = 5;
-int sailAngle = 80;
-int sailChange = 0;
+int sailAngle = 90;
+int sailChange = 1;
 Servo sail;
 
 int joyBtn = 1;
@@ -21,6 +23,10 @@ int lPin = 13;
 
 int xbeeRST = 2;
 
+int xbeeRSSI = 4;
+
+bool awake = false;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -28,69 +34,96 @@ void setup() {
 
   pinMode(rudderPin, OUTPUT);
   rudder.attach(rudderPin);
+  rudder.write(rudderAngle);
 
   pinMode(sailPin, OUTPUT);
   sail.attach(sailPin);
+  sail.write(sailAngle);
 
   pinMode(lPin, OUTPUT);
   digitalWrite(lPin, HIGH);
 
   pinMode(xbeeRST, OUTPUT);
   digitalWrite(xbeeRST, HIGH);
+
+  // Set pin as INPUT_PULLUP to avoid spurious wakeup
+//  pinMode(xbeeRSSI, INPUT_PULLUP);
+  // Attach a wakeup interrupt on pin 8, calling wakeup() when the device is woken up
+//  LowPower.attachInterruptWakeup(xbeeRSSI, wakeup, CHANGE);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available()) {
   
-    String msg = Serial.readString();
-    Serial.print(msg);
+  if (awake == false) {
+//    LowPower.sleep();
+  }
+  
+  if (Serial.available()) {
 
-    delay(10);
-
-    digitalWrite(xbeeRST, LOW);
-    delay(10);
-    digitalWrite(xbeeRST, HIGH);
-
-    
-    
-    char *cmd = strtok(msg.c_str(), ";");
-    char *lastCmd;
-
-    while (cmd != NULL) {
-      lastCmd = cmd;
-      cmd = strtok(NULL, ";");
+    char val = Serial.read();
+    switch(val)
+    {
+      case '-':
+        joyBtn = 1;
+        sail.write(sailAngle);
+        rudderChange = 0;
+        break;
+      
+      case 'e':
+        joyBtn = 0;
+        break;
+  
+      case 'E':
+        joyBtn = 1;
+        break;
+        
+      case 'S':
+        sail.write(sailAngle + sailChange * 8);
+        break;
+  
+      case 's':
+        sail.write(sailAngle - sailChange * 8);
+        break;
+  
+      case 'R':
+        rudderChange = -1;
+        break;
+  
+      case 'r':
+        rudderChange = 1;
+        break;
+  
     }
-
-
-    lastCmd = strtok(lastCmd, ",");
-    sailChange = atoi(lastCmd);
-
-    for (int i = 0; i < 2; i++) {
-      lastCmd = strtok(NULL, ",");
-      if (i == 0) {
-        rudderChange = atoi(lastCmd);
-      }
-      else if (i == 1) {
-        joyBtn = atoi(lastCmd);
-      }
-    }
-
+  
+    xbeeRefresh();
+    
   }
 
-  if (sailChange != 0) {
-    // Sail stuff
-    sailAngle += sailChange * servoChange;
+  awake = false;
 
-    if (sailAngle > 160) {
-      sailAngle = 160;
-    }
-    else if (sailAngle < 5) {
-      sailAngle = 5;
-    }
-    delay(cmdDT);
-  }
+//  if (sailChange != 0 && rudderChange != 0) {
+//    sailChange = 0;
+//  }
+
+//  if (sailChange != 0) {
+//    // Sail stuff
+////    sailAngle += sailChange * servoChange;
+//     sail.write(sailAngle + sailChange * 8);
+//
+//    if (sailAngle > 160) {
+//      sailAngle = 160;
+//    }
+//    else if (sailAngle < 5) {
+//      sailAngle = 5;
+//    }
+//    awake = true;
+//    delay(cmdDT);
+//  }
+//  else {
+//    sail.write(sailAngle);
+//  }
 
   if (rudderChange != 0) {
     // Rudder stuff
@@ -102,6 +135,7 @@ void loop() {
     else if (rudderAngle < 5) {
       rudderAngle = 5;
     }
+    awake = true;
     delay(cmdDT);
   }
 
@@ -110,10 +144,23 @@ void loop() {
     delay(rescueDT);
     rudder.write(125);
     delay(rescueDT);
+
+    awake = true;
   }
 
   rudder.write(rudderAngle);
 
-  sail.write(sailAngle);
+//  sail.write(sailAngle);
+  
+}
+
+void xbeeRefresh() {
+  delay(10);
+  digitalWrite(xbeeRST, LOW);
+  delay(10);
+  digitalWrite(xbeeRST, HIGH);
+}
+
+void wakeup() {
   
 }
